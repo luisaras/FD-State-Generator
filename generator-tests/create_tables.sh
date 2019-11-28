5af035bd2312a9119feb01b0ef9c3b59041b0959
Luísa Rocha <luisa.ras97@gmail.com>
2019-11-28 13:04:06 -0300

    planner outputs

--- generator-tests/create_tables.sh
+++ generator-tests/create_tables.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 table() 
 {
-    TASK=$1
-    if [ -f ${TASK}.dat ]; then
-        rm ${TASK}.dat
-    fi
+	TASK=$1/$2
+	if [ -f ${TASK}.dat ]; then
+		rm ${TASK}.dat
+	fi
 
-    for i in $(seq -w 1 10); do
+	for i in $(seq -w 1 10); do
 
-        GEN_OUTPUT=${TASK}/${i}_gen.txt
-        PLAN_OUTPUT=${TASK}/${i}_plan.txt
+		GEN_OUTPUT=${TASK}/${i}_gen.txt
+		PLAN_OUTPUT=${TASK}/${i}_plan.txt
+		PLAN_OUTPUT_OLD=planner/$2/${i}_plan.txt
 
-        EXPANDED=$(cat ${GEN_OUTPUT} | grep "Expanded" | tr -d "Expanded " | tr -d " state(s).")
-        SEARCH_TIME=$(cat ${GEN_OUTPUT} | grep "Search time" | tr -d "Search time: " | tr -d " s")
-        TOTAL_TIME=$(cat ${GEN_OUTPUT} | grep "Total time" | tr -d "Total time: " | tr -d " s")
-        H_OLD=$(cat ${GEN_OUTPUT} | grep "Original state h\-value" | tr -d "Original state h\-value: ")
-        H_NEW=$(cat ${GEN_OUTPUT} | grep "New state h\-value" | tr -d "New state h\-value: ")
-        LENGTH=$(cat ${PLAN_OUTPUT} | grep "Plan length" | tr -d "Plan length: " | tr -d " step(s).")
+		EXPANDED=$(grep "Expanded" ${GEN_OUTPUT} | tr -d "Expanded " | tr -d " state(s).")
+		SEARCH_TIME=$(grep "Search time" ${GEN_OUTPUT} | tr -d "Search time: " | tr -d " s")
+		TOTAL_TIME=$(grep "Total time" ${GEN_OUTPUT} | tr -d "Total time: " | tr -d " s")
+		H_OLD=$(grep "Original state" ${GEN_OUTPUT} | tr -d "Original state h\-value: ")
+		H_NEW=$(grep "New state" ${GEN_OUTPUT} | tr -d "New state h\-value: ")
+		
+		LENGTH_NEW=$(grep "Plan length" ${PLAN_OUTPUT} | tr -d "Plan length: " | tr -d " step(s).")
+		NODES_NEW=$(grep -m 1 "Expanded" ${PLAN_OUTPUT} | tr -d "Expanded " | tr -d " state(s).")
+		if [[ -z ${LENGTH_NEW} ]];
+		then
+			LENGTH_NEW="-"
+			NODES_NEW="-"
+		fi
 
-        if ! [[ -z ${LENGTH} ]];
-        then
-        	echo "$LENGTH	$H_OLD	$H_NEW	$EXPANDED	$SEARCH_TIME	$TOTAL_TIME" >> ${TASK}.dat
-        else
-        	echo "-	$H_OLD	$H_NEW	$EXPANDED	$SEARCH_TIME	$TOTAL_TIME" >> ${TASK}.dat
-        fi
-        
-    done
+		LENGTH_OLD=$(grep "Plan length" ${PLAN_OUTPUT_OLD} | tr -d "Plan length: " | tr -d " step(s).")
+		NODES_OLD=$(grep -m 1 "Expanded" ${PLAN_OUTPUT_OLD} | tr -d "Expanded " | tr -d " state(s).")
+		if [[ -z ${LENGTH_OLD} ]];
+		then
+			LENGTH_OLD="-"
+			NODES_OLD="-"
+		fi
+
+		echo "$2$i	$LENGTH_OLD	$LENGTH_NEW	$NODES_OLD	$NODES_NEW	$H_OLD	$H_NEW		$EXPANDED	$SEARCH_TIME	$TOTAL_TIME" >> ${TASK}.dat
+		
+	done
 }
 
-table lmcut/sokoban
-table lmcut/blocks
-table lmcut/airport
-table ipdb/sokoban
-table ipdb/blocks
-table ipdb/airport
-table perfect/sokoban
-table perfect/blocks
-table perfect/airport
-table novelty/sokoban
-table novelty/blocks
-table novelty/airport
+table lmcut sokoban
+table lmcut blocks
+table lmcut airport
+table ipdb sokoban
+table ipdb blocks
+table ipdb airport
+table perfect sokoban
+table perfect blocks
+table perfect airport
